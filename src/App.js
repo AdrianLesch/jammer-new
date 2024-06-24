a2d39f204893ab3c7c22a27dd1e33ecd0cc9d74e
Adrian Lesch <al.go.rhythmus@gmx.de>
2024-06-24 17:47:16 +0200

    Added search bar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@ import React, { useState } from 'react';
 import SearchResults from './SearchResults/SearchResults';
 import TRACK from './TRACK';
 
+function Search({getSearch}){
 
+  const [search, setSearch] = useState(null);
+
+  function handleChange(e){
+    setSearch(e.target.value);
+  }
+
+
+  function handleSubmit(){
+    getSearch(search);
+  }
+
+
+  return(
+    <>
+    <form onSubmit={handleSubmit} name="Search">
+      <input type="search" placeholder='Enter search here...' onChange={handleChange}/>
+      <button type="submit">Search</button>
+    </form>
+    </>
+  )
+}
 
 
 function App(){
 
-  const [search, setSearch] = useState([]);
+  const [track, setTrack] = useState("");
+
+  function getSearch(trackSearch){
+    setTrack(trackSearch);
+  }
+
+  console.log(track);
 
   return(
     <>
+    <Search getSearch={getSearch}/>
     <SearchResults tracks={TRACK} />
     </>
   )
--- src/TRACK.js
+++ src/TRACK.js
@@ -1,3 +1,3 @@
-const TRACK = [{ artist: "Steely Dan", song: "Ricky Don't Lose That Number", album: "Pretzel Logic" }, { artist: "Foo Fighters", song: "Everlong", album: "The Colour and the Shape" }];
+const TRACK = [{ artist: "Steely Dan", song: "Ricky Don't Lose That Number", album: "Pretzel Logic" }, { artist: "Foo Fighters", song: "Everlong", album: "The Colour and the Shape" }, { artist: "Tokyo Police Club ", song: "Nature of the Experiment", album: "A Lesson in Crime" }];
 
 export default TRACK;
