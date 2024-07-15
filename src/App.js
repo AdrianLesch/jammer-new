41fe56d83eb1d6a7b0e7295be783ab4ee317cce4
Adrian Lesch <al.go.rhythmus@gmx.de>
2024-07-15 16:28:15 +0200

    Started to extend SpotifyApi.js

--- src/App.js
+++ src/App.js
@@ -55,7 +55,3 @@ export default App;
 
 
 
-/*
-<MockBox savedPlaylist={savedPlaylist} />
- 
-  */
--- src/Spotify/SpotifyAPI.js
+++ src/Spotify/SpotifyAPI.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react';
 
 export default function SpotifyAPI({ sendSearch, getSearch}) {
     const [accessToken, setAccessToken] = useState("");
-    
+    const [userId, setUserId] = useState("");
 
     
     const CLIENT_ID = "3018fe9acf7d4fcfb25b96e1d2b84853";
     const CLIENT_SECRECT = "241ba7d436564a97b94efb0b5de1ba80";
 
+    //Authentication-fetching
     useEffect(() => {
         //API-Access Token / Authentication
         var authParameters = {
@@ -24,6 +25,7 @@ export default function SpotifyAPI({ sendSearch, getSearch}) {
             })
     }, [])
 
+    //GetSearch-Data
     useEffect(() => {
         var artistParameters = { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken } }
         var artistID = fetch('https://api.spotify.com/v1/search?q=' + sendSearch + '&type=track', artistParameters)
@@ -31,4 +33,16 @@ export default function SpotifyAPI({ sendSearch, getSearch}) {
         .then(data => getSearch(data.tracks.items[0]))
         .catch(error => console.log('Either nothing was searched for yet or the search was invalid'))
     }, [sendSearch]);
+
+    //GetUserId-fetching
+    useEffect(() => {
+        var userData = {method: 'GET', headers: {'Authorization': 'Bearer ' + accessToken }}
+        if (!accessToken){
+            alert("You are not authorized");
+        } else {
+        var currentUser = fetch('https://api.spotify.com/v1/me', userData)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        }
+    }, [accessToken]);
 }
