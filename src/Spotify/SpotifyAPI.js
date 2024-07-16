e07532870bf84140f6d1a80aad06bca68d121d5f
Adrian Lesch <al.go.rhythmus@gmx.de>
2024-07-16 18:11:10 +0200

    Still struggeling to get the SpotifyUserData

--- src/Spotify/SpotifyAPI.js
+++ src/Spotify/SpotifyAPI.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 
-export default function SpotifyAPI({ sendSearch, getSearch}) {
+export default function SpotifyAPI({ sendSearch, getSearch }) {
     const [accessToken, setAccessToken] = useState("");
     const [userId, setUserId] = useState("");
 
-    
+
     const CLIENT_ID = "3018fe9acf7d4fcfb25b96e1d2b84853";
     const CLIENT_SECRECT = "241ba7d436564a97b94efb0b5de1ba80";
 
@@ -16,7 +16,7 @@ export default function SpotifyAPI({ sendSearch, getSearch}) {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRECT + '&redirect_uri=http://localhost:3000'
+            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRECT + '&redirect_uri=http://localhost:3000/callback'
         }
         fetch('https://accounts.spotify.com/api/token', authParameters)
             .then(response => response.json())
@@ -29,20 +29,21 @@ export default function SpotifyAPI({ sendSearch, getSearch}) {
     useEffect(() => {
         var artistParameters = { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken } }
         var artistID = fetch('https://api.spotify.com/v1/search?q=' + sendSearch + '&type=track', artistParameters)
-        .then(response => response.json())
-        .then(data => getSearch(data.tracks.items[0]))
-        .catch(error => console.log('Either nothing was searched for yet or the search was invalid'))
+            .then(response => response.json())
+            .then(data => getSearch(data.tracks.items[0]))
+            .catch(error => console.log('Either nothing was searched yet or the search was invalid'))
     }, [sendSearch]);
 
     //GetUserId-fetching
     useEffect(() => {
-        var userData = {method: 'GET', headers: {'Authorization': 'Bearer ' + accessToken }}
-        if (!accessToken){
-            alert("You are not authorized");
-        } else {
-        var currentUser = fetch('https://api.spotify.com/v1/me', userData)
-        .then(response => response.json())
-        .then(data => console.log(data))
+        var userData = { method: 'GET', headers: { 'Authorization': 'Bearer ' +  accessToken } }
+        if (accessToken) {
+            var currentUser = fetch('https://api.spotify.com/v1/me', userData)
+                .then(response => response.json())
+                .then(data => console.log(data) )
         }
     }, [accessToken]);
 }
+
+
+
