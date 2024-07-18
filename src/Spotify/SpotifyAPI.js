2948a3c8e16cc7292eb9f400dbba0fc8d88845ac
Adrian Lesch <al.go.rhythmus@gmx.de>
2024-07-18 17:53:27 +0200

    Finally been able to get UserID

--- src/Spotify/SpotifyAPI.js
+++ src/Spotify/SpotifyAPI.js
@@ -4,31 +4,32 @@ export default function SpotifyAPI({ sendSearch, getSearch }) {
     const [accessToken, setAccessToken] = useState("");
     const [userId, setUserId] = useState("");
     
-   
-
     const CLIENT_ID = "3018fe9acf7d4fcfb25b96e1d2b84853";
     const CLIENT_SECRECT = "241ba7d436564a97b94efb0b5de1ba80";
 
     //Authentication-fetching
     useEffect(() => {
-        const scope = 'user-read-private user-read-email';
+        const hash = window.location.hash.substring(1);
+        const params = new URLSearchParams(hash);
+        const token = params.get('access_token');
     
-        var authUrl = 'https://accounts.spotify.com/authorize';
-        authUrl += '?response_type=token';
-        authUrl += '&client_id=' + encodeURIComponent(CLIENT_ID);
-        authUrl += '&redirect_uri=' + encodeURIComponent('http://localhost:3000/');
-        authUrl += '&scope=' + encodeURIComponent(scope);
-        
-        window.location = authUrl; 
-
-        setAccessToken(() => {const hash = window.location.hash.substring(1);
-            const params = new URLSearchParams(hash);
-            return params.get('access_token')});
+        if (token) {
+            setAccessToken(token);
+        } else {
+            const scope = 'user-read-private user-read-email';
+            var authUrl = 'https://accounts.spotify.com/authorize';
+            authUrl += '?response_type=token';
+            authUrl += '&client_id=' + encodeURIComponent(CLIENT_ID);
+            authUrl += '&redirect_uri=' + encodeURIComponent('http://localhost:3000/');
+            authUrl += '&scope=' + encodeURIComponent(scope);
+            
+            window.location = authUrl;
+        }
     }, []);
 
  
 
-    //GetSearch-Data -->>>>Das muss in die SearchBar Component auf onClick!!!
+    //GetSearch-Data 
     useEffect(() => {
         var artistParameters = { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken } }
         var artistID = fetch('https://api.spotify.com/v1/search?q=' + sendSearch + '&type=track', artistParameters)
@@ -47,26 +48,5 @@ export default function SpotifyAPI({ sendSearch, getSearch }) {
         }
     }, [accessToken]);
 }
-
-/*
-   useEffect(() => {
-        const scope = 'user-read-private user-read-email';
-    
-        var authUrl = 'https://accounts.spotify.com/authorize';
-        authUrl += '?response_type=token';
-        authUrl += '&client_id=' + encodeURIComponent(CLIENT_ID);
-        // Ensure this URI is exactly the same as the one registered in your Spotify app settings
-        authUrl += '&redirect_uri=' + encodeURIComponent('http://localhost:3000/');
-        authUrl += '&scope=' + encodeURIComponent(scope);
-        
-        window.location = authUrl; // Redirect user to Spotify authorization page
-
-        setAccessToken(() => {const hash = window.location.hash.substring(1);
-            const params = new URLSearchParams(hash);
-            return params.get('access_token')});
-    }, []);
-*/
-
-
 
 
